6bc9d3dcda7e153413c18bd5be5babafdead1663
Ard-Jan van Etten <ardjanve@gmail.com>
2024-05-07 09:32:25 +0200

    e

--- iframe.js
+++ iframe.js
@@ -4,3 +4,21 @@ window.addEventListener("message", (event) => {
         iframe.style = `max-height: ${event.data.value + 100}px`;
     }
 }, false);
+
+/*
+const el = document.querySelector("#content");
+
+const observer = new MutationObserver((mutationList, observer) => {
+    window.parent.postMessage({
+        type: "resize",
+        value: el.offsetHeight
+    }, "*");
+
+    console.log("resize", el.offsetHeight);
+}).observe(el, { attributes: true, childList: true, subtree: true });
+
+
+window.addEventListener("message", (event) => {
+    console.log(event.data.type);
+}, false);
+*/
