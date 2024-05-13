8c9397c4709198af760d9feb05865cfa722427ea
Ard-Jan van Etten <ardjanve@gmail.com>
2024-05-13 07:25:03 +0200

    e

--- iframe.js
+++ iframe.js
@@ -1,9 +1,9 @@
-window.addEventListener("message", (event) => {
-    if (event.data.type === "resize") {
-        const iframe = document.querySelector("iframe");
-        iframe.style = `max-height: ${event.data.value + 100}px`;
-    }
-}, false);
+// window.addEventListener("message", (event) => {
+//     if (event.data.type === "resize") {
+//         const iframe = document.querySelector("iframe");
+//         iframe.style = `max-height: ${event.data.value + 100}px`;
+//     }
+// }, false);
 
 /*
 const el = document.querySelector("#content");
@@ -22,3 +22,5 @@ window.addEventListener("message", (event) => {
     console.log(event.data.type);
 }, false);
 */
+window.addEventListener("message", (event) => { if (event.data.type === "resize") { const iframe = document.querySelector("iframe"); iframe.style = `max-height: ${event.data.value + 100}px`; }
+}, false);
